55b1fe647d814a0a733809f69f49272b0b673777
kamalakkannan R <kamalakkannan R>
2018-11-25 19:34:43 +0530

    yy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,  
+  apiUrl: 'http://fullstackdeveloper.co.in/mi/api/',
+  siteUrl: 'http://fullstackdeveloper.co.in/misite',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://fullstackdeveloper.co.in/mi/api/',  
+  siteUrl: 'http://fullstackdeveloper.co.in/misite',
 };
